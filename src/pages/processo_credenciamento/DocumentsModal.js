8398d074a36d36e13f36fe93cc85dde5289c1ab8
Grupo Nexus <ti@centroeducanexus.com.br>
2024-09-10 11:42:20 -0300

    refac: checklist do credenciamento funcionando e refatorado. list de status fatando

---
+++ src/pages/processo_credenciamento/DocumentsModal.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Modal, Table, Form, ProgressBar, Button } from 'react-bootstrap';
+
+const DocumentsModal = ({ show, onHide, documentos, instrucoes, onFileChange, onSubmitFile, progressoUpload }) => (
+    <Modal show={show} onHide={onHide} size="lg" centered>
+        <Modal.Header closeButton>
+            <Modal.Title>Anexos e Instruções</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <div className="row">
+                <div className="col-sm-6">
+                    <h4>Instruções</h4>
+                    <ul>
+                        {instrucoes.length > 0 ? (
+                            instrucoes.map((instrucao, index) => <li key={index}>{instrucao.descricao}</li>)
+                        ) : (
+                            <li>Nenhuma instrução encontrada.</li>
+                        )}
+                    </ul>
+                </div>
+                <div className="col-sm-6">
+                    <h4>Anexar um novo documento</h4>
+                    <Form onSubmit={onSubmitFile}>
+                        <Form.Group controlId="formFile" className="mb-3">
+                            <Form.Label>Escolha um arquivo</Form.Label>
+                            <Form.Control type="file" onChange={onFileChange} />
+                        </Form.Group>
+                        {progressoUpload > 0 && <ProgressBar now={progressoUpload} label={`${progressoUpload}%`} />}
+                        <Button variant="primary" type="submit">Salvar</Button>
+                    </Form>
+
+                    <hr />
+                    <h4>Anexos Enviados</h4>
+                    <Table responsive striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Anexo</th>
+                                <th>Status</th>
+                                <th>Observação</th>
+                                <th>Data do envio</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {documentos?.length > 0 ? (
+                                documentos.map((anexo, index) => (
+                                    <tr key={index}>
+                                        <td><a href={anexo.anexo} target="_blank" rel="noopener noreferrer">Visualizar</a></td>
+                                        <td>{anexo.status}</td>
+                                        <td>{anexo.observacao || ''}</td>
+                                        <td>{anexo.dataHoraCriacao}</td>
+                                    </tr>
+                                ))
+                            ) : (
+                                <tr>
+                                    <td colSpan="4">Nenhum documento encontrado</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </Table>
+                </div>
+            </div>
+        </Modal.Body>
+    </Modal>
+);
+
+export default DocumentsModal;
--- src/pages/processo_credenciamento/index.js
+++ src/pages/processo_credenciamento/index.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { Container, Row, Col, Form, Button, ProgressBar, Accordion, Card, Modal } from 'react-bootstrap';
+import React, { useState, useEffect, useContext } from 'react';
+import { Container, Row, Col, Accordion, Card, Button } from 'react-bootstrap';
 import Menu from '../../components/Menu';
 import AdminNavbar from '../../components/Navbar';
 import MainContent from '../../components/MainContent';
-import { uploadFile } from '../../services/uploadFile';
+import DocumentsModal from './DocumentsModal';
 import UserContext from '../../UserContext';
 import {
   buscaSolicitacaoDeCredenciamento,
@@ -11,263 +11,136 @@ import {
   listaDoChecklistDoEstado,
   listaDeInstrucoesDoChecklistApi,
 } from '../../services/credenciamento/credenciamentoService';
-import { FaFileAlt } from 'react-icons/fa';
-import { getToken } from '../../services/auth';
+import { uploadFile } from '../../services/uploadFile';
 
-class Index extends Component {
-  static contextType = UserContext;
+const Index = () => {
+  const userContext = useContext(UserContext);
+  const [idCredenciamento, setIdCredenciamento] = useState(0);
+  const [checklists, setChecklists] = useState([]);
+  const [documentos, setDocumentos] = useState([]);
+  const [instrucoes, setInstrucoes] = useState([]);
+  const [modalShow, setModalShow] = useState(false);
+  const [itemDochecklist, setItemDochecklist] = useState("");
+  const [progressoUpload, setProgressoUpload] = useState(0);
+  const [arquivo, setArquivo] = useState(null);
+  const [idChecklistCredenciamento, setIdChecklistCredenciamento] = useState(null); // Adicionando o estado
 
-  constructor(props) {
-    super();
-    this.state = {
-      id_credenciamento: 0,
-      arquivo: null,
-      progressoUpload: 0,
-      arrayDocumentosDoCredenciamento: [],
-      arrayChecklistCredenciamento: [],
-      arrayInstrucoesDoChecklist: [],
-      id_checklist_credenciamento: 0,
-      success: '',
-      error: '',
-      modalShowCadastrarAnexo: false,
-      itemDochecklist: "",
-    };
-  }
-
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  fetchData = async () => {
-    const userContext = this.context;
-    const idUsuario = userContext.user.id;
-
-    try {
-      const credenciamentoData = await buscaSolicitacaoDeCredenciamento(idUsuario);
-      if (credenciamentoData && credenciamentoData.resultados && credenciamentoData.resultados[0]) {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const idUsuario = userContext.user.id;
+        const credenciamentoData = await buscaSolicitacaoDeCredenciamento(idUsuario);
         const credenciamento = credenciamentoData.resultados[0];
-        this.setState({
-          id_credenciamento: credenciamento.id_credenciamento,
-        });
-        this.listaDedocumentosDoCredenciamento(credenciamento.id_credenciamento);
-        this.listaDoChecklistDoEstado(credenciamento.id_estado);
+        setIdCredenciamento(credenciamento.id_credenciamento);
+        console.log('Credenciamento:', credenciamento);
+
+        // Busca checklists do estado
+        await listaDoChecklistDoEstado(credenciamento.id_estado).then(setChecklists);
+      } catch (error) {
+        console.error('Erro ao buscar credenciamento:', error);
       }
-    } catch (error) {
-      console.error('Erro ao buscar credenciamento:', error);
-    }
-  };
+    };
 
-  // Corrigido para receber ambos os parâmetros
-  listaDedocumentosDoCredenciamento = async (id_credenciamento, id_checklist_credenciamento = null) => {
+    fetchData();
+  }, [userContext]);
+
+  const handleShowModal = async (checklist) => {
+    setItemDochecklist(checklist.nome);
+    setIdChecklistCredenciamento(checklist.id_checklist); // Armazenar id_checklist_credenciamento
+    setModalShow(true);
+  
     try {
-      const response = await listaDedocumentosDoCredenciamentoApi(id_checklist_credenciamento, id_credenciamento);
-      console.log('Documentos recebidos:', response);
-
-      if (response.status === 200) {
-        this.setState({ arrayDocumentosDoCredenciamento: response.resultados });
+      // Buscando as instruções do checklist
+      const instrucoesData = await listaDeInstrucoesDoChecklistApi(checklist.id_checklist);
+      setInstrucoes(instrucoesData);
+  
+      // Buscando os documentos anexados para o checklist e credenciamento
+      const documentosData = await listaDedocumentosDoCredenciamentoApi(checklist.id_checklist, idCredenciamento);
+      console.log('Documentos recebidos:', documentosData);
+  
+      if (documentosData.status === 200 && documentosData.resultados && documentosData.resultados.length > 0) {
+        setDocumentos(documentosData.resultados); // Acessando corretamente os resultados
       } else {
-        console.error('Erro ao carregar documentos:', response.msg);
+        console.log('Nenhum documento encontrado.');
+        setDocumentos([]); // Certifique-se de limpar o estado caso não haja documentos
       }
     } catch (error) {
-      console.error('Erro ao buscar documentos:', error);
+      console.error('Erro ao buscar documentos ou instruções:', error);
     }
   };
+  
 
-  listaDoChecklistDoEstado = async (id_estado) => {
-    try {
-      const checklist = await listaDoChecklistDoEstado(id_estado);
-      this.setState({ arrayChecklistCredenciamento: checklist });
-    } catch (error) {
-      console.error('Erro ao listar checklist de credenciamento:', error);
-    }
+  const handleFileChange = (e) => {
+    setArquivo(e.target.files[0]);
   };
 
-  onChangeFileInput = (e) => {
-    this.setState({ arquivo: e.target.files[0] });
-  };
-
-  cadastrarDocumentoDoCredenciamento = async (e) => {
+  const handleFileUpload = async (e) => {
     e.preventDefault();
-
-    const { arquivo, id_credenciamento } = this.state;
-    if (!arquivo) {
-      this.setState({ error: 'Por favor, selecione um arquivo para anexar.' });
-      return;
-    }
-
     try {
-      await uploadFile(arquivo, `nexus/credenciamento/`, this.onProgress);
-      this.listaDedocumentosDoCredenciamento(id_credenciamento);
-      this.setState({ success: 'Arquivo enviado com sucesso!', progressoUpload: 0 });
+      if (!arquivo) {
+        console.error('Nenhum arquivo selecionado.');
+        return;
+      }
+
+      await uploadFile(arquivo, `nexus/credenciamento/`, setProgressoUpload);
+      await listaDedocumentosDoCredenciamentoApi(idChecklistCredenciamento, idCredenciamento); // Usar idChecklistCredenciamento
+      setProgressoUpload(0); // Resetar progresso após upload
     } catch (error) {
-      this.setState({ error: 'Erro ao enviar documento. Tente novamente.', progressoUpload: 0 });
       console.error('Erro ao enviar documento:', error);
     }
   };
 
-  onProgress = (percentCompleted) => {
-    this.setState({ progressoUpload: percentCompleted });
-  };
+  return (
+    <Container fluid style={{ padding: '0px', minHeight: '100vh' }}>
+      <Menu />
+      <Row>
+        <Col xs={12}>
+          <AdminNavbar />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} id="main">
+          <MainContent>
+            <Accordion>
+              <Card>
+                <Accordion.Item eventKey="1">
+                  <Accordion.Header>Checklist do Credenciamento</Accordion.Header>
+                  <Accordion.Body>
+                    <Row>
+                      {checklists.length > 0 ? (
+                        checklists.map((checklist, index) => (
+                          <Col sm={4} key={index}>
+                            <Card className="text-center font-weight-bold">
+                              <Card.Header>{index + 1} - {checklist.nome}</Card.Header>
+                              <Card.Body>
+                                <Button onClick={() => handleShowModal(checklist)}>Anexos e Instruções</Button>
+                              </Card.Body>
+                            </Card>
+                          </Col>
+                        ))
+                      ) : (
+                        <Col>Nenhum checklist encontrado</Col>
+                      )}
+                    </Row>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Card>
+            </Accordion>
 
-  handlerShowModalCadastrarAnexo = (checklistCredenciamento) => {
-    if (!checklistCredenciamento) return;
-    this.setState({
-      modalShowCadastrarAnexo: true,
-      itemDochecklist: checklistCredenciamento.nome,
-      id_checklist_credenciamento: checklistCredenciamento.id_checklist,
-    });
-    this.listaDeInstrucoesDoChecklist(checklistCredenciamento.id_checklist);
-    this.listaDedocumentosDoCredenciamento(this.state.id_credenciamento, checklistCredenciamento.id_checklist);
-  };
-
-  handlerCloseModalCadastrarAnexo = () => {
-    this.setState({ modalShowCadastrarAnexo: false });
-  };
-
-  listaDeInstrucoesDoChecklist = async (id_checklist) => {
-    try {
-      const instrucoes = await listaDeInstrucoesDoChecklistApi(id_checklist);
-      this.setState({ arrayInstrucoesDoChecklist: instrucoes });
-    } catch (error) {
-      console.error('Erro ao listar instruções do checklist:', error);
-    }
-  };
-
-  render() {
-    const {
-      progressoUpload,
-      arrayDocumentosDoCredenciamento,
-      arrayChecklistCredenciamento,
-      arrayInstrucoesDoChecklist,
-      success,
-      error,
-      modalShowCadastrarAnexo,
-      itemDochecklist,
-    } = this.state;
-
-    return (
-      <Container fluid style={{ padding: '0px', minHeight: '100vh' }}>
-        <Menu />
-        <Row>
-          <Col xs={12}>
-            <AdminNavbar id_usuario={this.state.id_usuario} />
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12} id="main">
-            <MainContent>
-              <Accordion>
-                <Card>
-                  <Accordion.Item eventKey="0">
-                    <Accordion.Header>Informações da Solicitação</Accordion.Header>
-                    <Accordion.Body>
-                      {/* Detalhes da solicitação aqui */}
-                    </Accordion.Body>
-                  </Accordion.Item>
-                </Card>
-
-                <Card>
-                  <Accordion.Item eventKey="1">
-                    <Accordion.Header>Checklist do Credenciamento</Accordion.Header>
-                    <Accordion.Body>
-                      <div style={{ height: '350px', overflowY: 'scroll', padding: '30px' }}>
-                        <Row>
-                          {arrayChecklistCredenciamento.length > 0 ? (
-                            arrayChecklistCredenciamento.map((checklistCredenciamento, index) => (
-                              <Col sm={4} key={checklistCredenciamento.id_checklist}>
-                                <Card className="text-center font-weight-bold zoom" style={{ backgroundColor: 'rgba(255, 255, 255, 0.3)', height: '200px', border: '1px solid #000233' }}>
-                                  <Card.Header style={{ height: '60px' }}>{index + 1} - {checklistCredenciamento.nome}</Card.Header>
-                                  <Card.Body>
-                                    <Button onClick={() => this.handlerShowModalCadastrarAnexo(checklistCredenciamento)}>
-                                      Anexos e Instruções
-                                    </Button>
-                                  </Card.Body>
-                                </Card>
-                              </Col>
-                            ))
-                          ) : (
-                            <Col>Nenhum checklist encontrado</Col>
-                          )}
-                        </Row>
-                      </div>
-                    </Accordion.Body>
-                  </Accordion.Item>
-                </Card>
-              </Accordion>
-
-              <Modal show={modalShowCadastrarAnexo} onHide={this.handlerCloseModalCadastrarAnexo} size="lg" centered>
-                <Modal.Header closeButton>
-                  <Modal.Title>Anexos do {itemDochecklist}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                  <div className="row">
-                    <div className="col-sm-6">
-                      <h4>Instruções</h4>
-                      <ul>
-                        {arrayInstrucoesDoChecklist.length > 0 ? (
-                          arrayInstrucoesDoChecklist.map((instrucao, index) => (
-                            <li key={index}>{instrucao.descricao}</li>
-                          ))
-                        ) : (
-                          <li>Nenhuma instrução encontrada.</li>
-                        )}
-                      </ul>
-                    </div>
-                    <div className="col-sm-6">
-                      <h4>Anexar um novo documento</h4>
-                      <Form onSubmit={this.cadastrarDocumentoDoCredenciamento}>
-                        <Form.Group controlId="formFile" className="mb-3">
-                          <Form.Label>Escolha um arquivo</Form.Label>
-                          <Form.Control type="file" onChange={this.onChangeFileInput} />
-                        </Form.Group>
-                        {progressoUpload > 0 && (
-                          <ProgressBar now={progressoUpload} label={`${progressoUpload}%`} />
-                        )}
-                        <Button variant="primary" type="submit">Salvar</Button>
-                      </Form>
-                      {success && <div className="alert alert-success mt-3">{success}</div>}
-                      {error && <div className="alert alert-danger mt-3">{error}</div>}
-
-                      <hr />
-                      <h4>Anexos já enviados</h4>
-                      <div className="table-responsive table-sm">
-                        <table className="table table-bordered table-hover text-center table-light">
-                          <thead className="thead-light">
-                            <tr>
-                              <th scope="col">Anexo</th>
-                              <th scope="col">Status</th>
-                              <th scope="col">Observação</th>
-                              <th scope="col">Data do envio</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            {arrayDocumentosDoCredenciamento.length > 0 ? (
-                              arrayDocumentosDoCredenciamento.map((anexo, index) => (
-                                <tr key={index} className={anexo.id_status === 4 ? 'table-danger' : anexo.id_status === 3 ? 'table-success' : ''}>
-                                  <td><a href={anexo.anexo} target="_blank" rel="noopener noreferrer">Visualizar</a></td>
-                                  <td>{anexo.status}</td>
-                                  <td>{anexo.observacao || ''}</td>
-                                  <td>{anexo.dataHoraCriacao}</td>
-                                </tr>
-                              ))
-                            ) : (
-                              <tr>
-                                <td colSpan="4">Nenhum documento encontrado</td>
-                              </tr>
-                            )}
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>
-                  </div>
-                </Modal.Body>
-              </Modal>
-            </MainContent>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+            <DocumentsModal
+              show={modalShow}
+              onHide={() => setModalShow(false)}
+              documentos={documentos? documentos : []}
+              instrucoes={instrucoes}
+              onFileChange={handleFileChange}
+              onSubmitFile={handleFileUpload}
+              progressoUpload={progressoUpload}
+            />
+          </MainContent>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Index;
